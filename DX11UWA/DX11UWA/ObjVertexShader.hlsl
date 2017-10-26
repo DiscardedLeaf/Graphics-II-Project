23540fd3783d38568e479946438148e47d5355a0
David Crane <DBCrane@fullsail.edu>
2017-10-26 15:35:22 -0400

    Filled out vertex shader

--- DX11UWA/DX11UWA/ObjVertexShader.hlsl
+++ DX11UWA/DX11UWA/ObjVertexShader.hlsl
@@ -1,4 +1,13 @@
+cbuffer ModelViewProjectionConstantBuffer : register(b0)
+{
+	matrix model;
+	matrix view;
+	matrix projection;
+};
+
+
 float4 main( float4 pos : POSITION ) : SV_POSITION
 {
+	
 	return pos;
 }
